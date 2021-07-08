3c54a88c02fdf51e08cf34ef3110dda6ea8a1484
NithaMurali <33086322+NithaMurali@users.noreply.github.com>
2021-07-08 20:50:50 +0530

    Added MSTest project and code refactor

--- BusinessRuleEngineApp/BusinessRuleEngineApp/Program.cs
+++ BusinessRuleEngineApp/BusinessRuleEngineApp/Program.cs
@@ -1,8 +1,4 @@
-﻿using BusinessRuleEngineApp.Data;
-using BusinessRuleEngineApp.Data.Interfaces;
-using BusinessRuleEngineApp.Data.Products;
-using BusinessRuleEngineApp.Rules;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,56 +8,18 @@ namespace BusinessRuleEngineApp
 {
     class Program
     {
-       public static List<IItem> cartItems;
+       
         static void Main(string[] args)
         {
             //add elements to cart
-            Program.InitializeProducts();
+            PaymentEngine.InitializeProducts();
             //process payments for items in cart
-            Program.ProcessPayments();
+            PaymentEngine.ProcessPayments();
             Console.ReadLine();
 
         }
 
-        /*Arguments: None
-         Description: Adds products to the cart*/
-        public static void InitializeProducts()
-        {
-            cartItems = new List<IItem>();
-            IItem phyProdItem = new PhysicalProduct("Physical Product");
-            phyProdItem.AddRule(new PhysicalProductRule());
-            phyProdItem.AddRule(new CommisionPaymentRule());
-            cartItems.Add(phyProdItem);
-
-            IItem videoItem = new Video("Learning to Ski");
-            videoItem.AddRule(new VideoRule());
-            cartItems.Add(videoItem);
-
-            IItem bookItem = new Book("Book");
-            bookItem.AddRule(new BookRule());
-            bookItem.AddRule(new CommisionPaymentRule());
-            cartItems.Add(bookItem);
-
-            IItem newMembershipItem = new NewMembership("New Registration");
-            newMembershipItem.AddRule(new NewMembershipRule());
-            newMembershipItem.AddRule(new EmailRule());
-            cartItems.Add(newMembershipItem);
-
-            IItem upgradeMembershipItem = new UpgradeMembership("Upgrade Membership");
-            upgradeMembershipItem.AddRule(new UpgradeMembershipRule());
-            upgradeMembershipItem.AddRule(new EmailRule());
-            cartItems.Add(upgradeMembershipItem);
-
-        }
-
-        public static void ProcessPayments()
-        {
-            //After payment processing, apply the payment rule for each item in cart
-            foreach(IItem item in cartItems)
-            {
-                item.applyRule();
-            }
-        }
+       
     }
    
 }
---
+++ BusinessRuleEngineApp/BusinessRuleEngineTestProject/ProgramTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRuleEngineTestProject
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Test_InitializeProducts()
+        {
+            
+        }
+    }
+}
