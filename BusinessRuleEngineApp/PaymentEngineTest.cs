0f88737a79811afc957a111dfb7dcaef4a057263
NithaMurali <33086322+NithaMurali@users.noreply.github.com>
2021-07-08 21:12:47 +0530

    update testclass

---
+++ BusinessRuleEngineApp/PaymentEngineTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessRuleEngineApp;
+
+namespace BusinessRuleEngineTestProject
+{
+    [TestClass]
+    public class PaymentEngineTest
+    {
+        [TestMethod]
+        public void Test_InitializeProducts()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            PaymentEngine.InitializeProducts();
+        }
+    }
+}
